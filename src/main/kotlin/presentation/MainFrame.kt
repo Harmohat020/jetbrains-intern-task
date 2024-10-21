8dc508fc0b45b5a74943e61742f7b8e44b9889ef
Harmohat <harmohat_02@live.com>
2024-10-21 17:42:27 +0200

    feat: created UI

--- src/main/kotlin/presentation/MainFrame.kt
+++ src/main/kotlin/presentation/MainFrame.kt
@@ -1,4 +1,39 @@
 package presentation
 
-class MainFrame {
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Dimension
+import javax.swing.*
+
+class MainFrame : JFrame() {
+    private val componentWidth = 200
+    val loadFileButton = customButton("Load file")
+    val textArea = JTextArea().apply {
+        isEditable = false
+        lineWrap = true
+        wrapStyleWord = true
+    }
+
+    private val scrollPane = JScrollPane(textArea)
+
+    init {
+        layout = BorderLayout()
+        add(loadFileButton, BorderLayout.NORTH)
+        add(scrollPane, BorderLayout.CENTER)
+
+        extendedState = MAXIMIZED_BOTH
+        defaultCloseOperation = EXIT_ON_CLOSE
+    }
+
+    private fun customButton(text: String): JButton {
+        return JButton(text).apply {
+            preferredSize = Dimension(componentWidth, 45)
+            isBorderPainted = false
+        }
+    }
+
+    fun showFileContent(content: String) {
+        textArea.text = content
+    }
+
 }
