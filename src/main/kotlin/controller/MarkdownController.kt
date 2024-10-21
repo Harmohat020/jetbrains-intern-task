12ef5a71e79b5d3ab42b8293cf4f9769af8e583b
Harmohat <harmohat_02@live.com>
2024-10-21 19:49:38 +0200

    feat: implemented MarkdownController to handle file loading and validation

--- src/main/kotlin/controller/MarkdownController.kt
+++ src/main/kotlin/controller/MarkdownController.kt
@@ -1,4 +1,31 @@
 package controller
 
-class MarkdownController {
+import model.MarkdownModel
+import presentation.MainFrame
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import javax.swing.JFileChooser
+import javax.swing.JOptionPane
+import javax.swing.filechooser.FileNameExtensionFilter
+
+class MarkdownController(private val model: MarkdownModel, private val frame: MainFrame) {
+    init {
+        frame.loadFileButton.addActionListener(object: ActionListener {
+            override fun actionPerformed(e: ActionEvent?) {
+                val fileChooser = JFileChooser()
+                fileChooser.fileFilter = FileNameExtensionFilter("Markdown file", "md")
+
+                if (fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                    val file = fileChooser.selectedFile
+
+                    if (model.validateFile(file)) {
+                        val content = model.loadFile(file)
+                        frame.showFileContent(content)
+                    } else {
+                        JOptionPane.showMessageDialog(frame, "Invalid file type, please select a .md file.")
+                    }
+                }
+            }
+        })
+    }
 }
