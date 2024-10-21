85d7af6b6f855ad625c0a0b9f496940d5d0bd283
Harmohat <harmohat_02@live.com>
2024-10-21 19:50:22 +0200

    feat: initialized setup with MainFrame, MarkdownModel, and MarkdownController

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
+import controller.MarkdownController
+import model.MarkdownModel
+import presentation.MainFrame
 
 fun main(args: Array<String>) {
+    val mainFrame = MainFrame()
+    val markdownModel = MarkdownModel()
+    val markdownController = MarkdownController(markdownModel, mainFrame)
 
+    mainFrame.isVisible = true
 }
