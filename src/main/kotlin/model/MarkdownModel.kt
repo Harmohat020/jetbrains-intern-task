34e96be8c9af816dd93f22efeda3820c2a12d610
Harmohat <harmohat_02@live.com>
2024-10-21 17:38:45 +0200

    feat: added file loading and validation methods in MarkdownModel

--- src/main/kotlin/model/MarkdownModel.kt
+++ src/main/kotlin/model/MarkdownModel.kt
@@ -1,4 +1,14 @@
 package model
 
+import java.io.File
+import java.nio.file.Files
+
 class MarkdownModel {
+    fun loadFile(file: File): String {
+        return Files.readAllLines(file.toPath()).joinToString("\n")
+    }
+
+    fun validateFile(file: File): Boolean {
+        return file.extension.equals("md", ignoreCase = true)
+    }
 }
